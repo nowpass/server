394d5b83184c651e0a655976b69c87826fd59d45
Yves Hoppe <yves.hoppe@community.joomla.org>
2018-01-15 10:13:01 +0100

    Added a basic design for the homepage

---
+++ scripts/sample-data.sql
@@ -0,0 +1,27 @@
+INSERT INTO nowpass.`element` (createdAt,updatedAt,id,user_id,kind,`group`,title,url,username,password,form_data,comment,status) VALUES 
+(1516003052202,1516003052202,1,1,'website','','Twitter Account','https://twitter.com','johnsmith','oeyDom5BPD457hH6DINvOMrAUiGJIfwj3Xi2HF/0xRPRKYsEKe7xnHLsUpm7WWcV','','',1)
+,(1516003069207,1516003069207,2,1,'website','','Facebook','https://facebook.com','johnsmith','jQNaM/7CvVwwrGvxKS1FjYGwzcpLBbyLna39pLLYia4AuwLVK7xiBAYSCZ6OhPTR','','',1)
+,(1516003092290,1516003092290,3,1,'website','','Personal Google Account','https://accounts.google.com','john.smith@gmail.com','CZY9MQIfIwHlb+zzYkk2asNsPdmzr+AvtmpO/uhZHGvoEeSACnexy5G0iLD6eqTP','','',1)
+,(1516003170330,1516003170330,4,1,'website','','Business Google Account','https://accounts.google.com','john.smith@compojoom.com','f0d6KJbvMP075Qg5LuJH13QN0qMMHYCEfYD6KNN6868SAJDq50PLG0a/06QHqccH','','',1)
+,(1516003188373,1516003188373,5,1,'website','','Instagram','https://instagram.com','john.smith','v9ckSpDrFiGybYb19Z9tlwtdlc7PF4tHUoAenRKw80EigLR83QXyx0zaDzU6QkC/','','',1)
+,(1516003260183,1516003260183,6,1,'website','','123rf','https://www.123rf.com','john.smith','MLcahNksCaM0wF/Mywo2+knfG3cTlooybi1DzK30rFUz5WBEJj6611CjCsPMaw0t','','',1)
+,(1516003292200,1516003292200,7,1,'website','','1and1','https://1and1.com','424242','B/LCNjIFctCcc6PHjSeo+PiPyxvUK5Zlpix+B6Vr80ylojr6JTLFuUZjT8mNYTAe','','',1)
+,(1516003321145,1516003321145,8,1,'website','','Adobe Business Account','https://adobe.com','john.smith@gmail.com','3581CS1S+0c2m1RUG20PMQ34tG9KxTuRPY2U6yhe67KQLdC9KDA06E1jBEpECQn4','','',1)
+,(1516003342896,1516003342896,9,1,'website','','Amazon Personal','https://amazon.com','john.smith@gmail.com','KtUitv6xgWKCjPzdIp13i1e53RctMJpvqf5qDjzeAUP/d+uwk5DYdtZNm17Bu8KB','','',1)
+,(1516003365537,1516003365537,10,1,'website','','Apple Account','https://appleid.apple.com','john.smith@gmail.com','bkC+jaaz3QhGVxsO7H0VU1hKats0ZuEJ3xW7nQZ9ftcpz0BfWeveBvofgY64eo3j','','',1)
+;
+INSERT INTO nowpass.`element` (createdAt,updatedAt,id,user_id,kind,`group`,title,url,username,password,form_data,comment,status) VALUES 
+(1516004011140,1516004011140,11,1,'website','','Asana','https://www.asana.com','john.smith@gmail.com','9n5q55e/DmAuYgWxofY2z+TsUZqsXs6Cl0kgkQgp5QGsP/nZgU0c1azAskmat6cF','','',1)
+,(1516004038712,1516004038712,12,1,'website','','DELL','https://www.dell.com','john.smith@gmail.com','aMx45asEbGN/Cu0ZncdyPGa4tFXvnU7Fjt6GbtHuiissUJOYaql2X2wonPqY5bZN','','',1)
+,(1516004064516,1516004064516,13,1,'website','','Docker','https://hub.docker.com','john.smith@gmail.com','uPMqVUOuTjPSFKIAk+WIQp0pjqgeWeHnxNorKu/CbE96RDhOID9lRHAsGdeh66kN','','',1)
+,(1516004081535,1516004081535,14,1,'website','','Dropbox','','john.smith@gmail.com','SWWvQXvG14Nmo3PDKt07REjgb+2IvkNEFhAvWHVIyLS+XN7bNdsYfSjfQKbJn1li','','',1)
+,(1516004095677,1516004095677,15,1,'website','','Ebay','https://ebay.com','john.smith@gmail.com','atvu4nao8/CdnMF6CPb9ZCZSXmzulVN+LvECZi0NR2ssDch3O8cAxTi6OsVKxCig','','',1)
+,(1516004119980,1516004119980,16,1,'website','','Envato','https://envato.com','john.smith','kV0qely1rno/dg/AMP/umJApE53zVaEgTDcuH8EkIs9oREUg0sWNAAa2mAT1EmGk','','',1)
+,(1516004143941,1516004143941,17,1,'website','','GitHub','https://github.com','johnsmith','KU8oeK3G71yOJ3QkyCG+lKsYxGN85syabUlkPrYEi5qKtZXTRFS6cx50R7B+eh0B','','',1)
+,(1516004160804,1516004160804,18,1,'website','','GitLab','https://gitlab.com','johns','fGBADPEypTJ9S8Cl2wwhx8jDYhSW9XEM8blHj7oQaKTAaGyccebGhEFKw3PTQcq7','','',1)
+,(1516004203102,1516004203102,19,1,'website','','Netflix','https://www.netflix.com','john.smith@gmail.com','cz/erIr/mEyxDS9xzPyw5ylETRHfrVouix9RN4vuyVYoeJPy2ZboqlBaAq4og61q','','',1)
+,(1516004233555,1516004233555,20,1,'website','','Samsung','https://samsung.com','john.smith@gmail.com','LJDz/9jEVipqaPBc/jUUIBm6cNEaMFDq/BmZLNt+IJL9e7B0E9imNLQh5ntgr9Mc','','',1)
+;
+INSERT INTO nowpass.`element` (createdAt,updatedAt,id,user_id,kind,`group`,title,url,username,password,form_data,comment,status) VALUES 
+(1516004255361,1516004255361,21,1,'website','','Snort','https://www.snort.org','john.smith@gmail.com','zj39N8KU4IxvhgfOItT7b9PvZ2CHlDBxv7Y9QWt9GmF5tE4W3mkzbZF0NRkQ5RuT','','',1)
+;
